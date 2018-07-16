5b2dfd85b34d143226286a5d17b2697e60c86448
Vandana <mitt.vandu@gmail.com>
2018-07-15 23:33:30 -0500

    Chap8, Outliers, Quiz 14 to 17
    Removed an outlier and visualized the data after removal

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -10,10 +10,23 @@ from feature_format import featureFormat, targetFeatureSplit
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
+
+# Remove a key-value pair from a dictionary is the following line: dictionary.pop( key, 0 )
+# In our data TOTAL was the key which we want to pop out
+data_dict.pop( 'TOTAL' )
 data = featureFormat(data_dict, features)
+
+for i in data_dict:
+    print i
 
 
 ### your code below
 
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
 
-
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
