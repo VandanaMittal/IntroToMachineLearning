273ddfff6ca2c2aa605872c63b276acba9e50ac6
Vandana <mitt.vandu@gmail.com>
2018-07-29 02:39:15 -0500

    Chap11, TextLearning, Quiz 19
    Extracted the emails, removed some signature words, identified whether the email is from sara or chris

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -44,21 +44,34 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
         temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
-            print path
+            #print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            email_text = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
+            email_text.replace("sara", " ")
+            email_text.replace("shackleton", " ")
+            email_text.replace("chris", " ")
+            email_text.replace("germani", " ")
             ### ["sara", "shackleton", "chris", "germani"]
 
             ### append the text to word_data
+            word_data.append(email_text)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+
+            if name == "sara":
+                from_data.append(0)
+            else:
+                from_data.append(1)
+
 
 
             email.close()
 
+print word_data[152]
 print "emails processed"
 from_sara.close()
 from_chris.close()
@@ -71,5 +84,3 @@ pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
 ### in Part 4, do TfIdf vectorization here
-
-
