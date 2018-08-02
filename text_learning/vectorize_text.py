c81d58dcb0cbdd569e044e57b7762e5e661f18ad
Vandana <mitt.vandu@gmail.com>
2018-08-01 21:43:53 -0500

    Chap11, TextLearning, Quiz 20 and 21
    End of the lesson

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -41,8 +41,8 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
-        if temp_counter < 200:
+        #temp_counter += 1
+        #if temp_counter < 200:
             path = os.path.join('..', path[:-1])
             #print path
             email = open(path, "r")
@@ -51,10 +51,10 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
             email_text = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
-            email_text.replace("sara", " ")
-            email_text.replace("shackleton", " ")
-            email_text.replace("chris", " ")
-            email_text.replace("germani", " ")
+            email_text.replace("sara", "")
+            email_text.replace("shackleton", "")
+            email_text.replace("chris", "")
+            email_text.replace("germani", "")
             ### ["sara", "shackleton", "chris", "germani"]
 
             ### append the text to word_data
@@ -71,7 +71,13 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
 
             email.close()
 
+#print from_data
 print word_data[152]
+
+'''from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer()
+vectorizer.fit_transform(word_data)'''
+
 print "emails processed"
 from_sara.close()
 from_chris.close()
@@ -81,6 +87,20 @@ pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
 
-
-
 ### in Part 4, do TfIdf vectorization here
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# We got the hint to load this pickle file "real_your_word_data" from the following link.
+# The correct number of words are 38757, but using this file we get 38755 words.
+# https://discussions.udacity.com/t/lesson-11-quiz-20-21/636246
+word_data = pickle.load(open("real_your_word_data.pkl.txt", "rb"))
+print "len:", len(word_data)
+
+transformer = TfidfVectorizer(stop_words="english")
+word_data_trans = transformer.fit_transform(word_data)
+word_list = transformer.get_feature_names()
+
+print "Length of word_list: ", len(word_list)
+print "Word Number 34597: ", word_list[34597]
