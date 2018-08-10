d105735d03df19107af0de9256c44aa877d71b5f
Vandana <mitt.vandu@gmail.com>
2018-08-09 23:25:42 -0500

    Chap15, Quiz 30
    If your identifier predicted 0. (not POI) for everyone in the test set, then accuracy is changed

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -41,19 +41,22 @@ pred = clf.predict(X_test)
 # Quiz 28 : How many POIs are predicted for the test set for your POI identifier?
 # Solution : If the the person in the predicted data is POI then the value in pred
 # for that person is 1, otherwise 0.
+print "the number of predicted POIs are :", sum(pred)  # ans = 4
 
-print "the number of POIs are :", sum(pred)  # ans = 4
+# Quiz 29 : How many people total are in your test set?
+print "the total people in test set are :", len(pred) # ans = 29
+acc = accuracy_score(y_test, pred)
+print("The accuracy on test data is :", acc) # accuracy =  0.724
 
+# Quiz 30 : If your identifier predicted 0. (not POI) for everyone in the test set,
+# what would its accuracy be?
 count = 0
 for i in pred:
     count = count + 1
     if i==1:
-        print i
+        #print i
         print count
-
-# Quiz 29 : How many people total are in your test set?
-print "the total people in test set are :", len(pred) # ans = 29
-
-
+        # set the predicted POI as 0, the accuracy is improved to 86.2%
+        pred[count-1] = 0 # 0.862
 acc = accuracy_score(y_test, pred)
-print("The accuracy on test data is :", acc)
+print("The accuracy on test data after replacing POI as 0 is :", acc) # accuracy = 0.862
